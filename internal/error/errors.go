852fe5e435a9b4947ea9f2449d59e2d0b945bdd3
zt <zt@gmail.com>
2025-12-02 23:05:28 +0800

    改m

--- internal/error/errors.go
+++ internal/error/errors.go
@@ -3,6 +3,7 @@ package error
 import "errors"
 
 var (
-	ErrInvalidRequest error = errors.New("无效的请求")
-	ErrRecordNotFound error = errors.New("记录不存在")
+	ErrInvalidRequest            error = errors.New("无效的请求")
+	ErrRecordNotFound            error = errors.New("记录不存在")
+	GroupSendBotNotSupportFeuter error = errors.New("群发机器人不支持配置功能")
 )
